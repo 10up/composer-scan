6e0c239b9673aab77721f883922474b78ab2babb
Jason DeWitt <jason.dewitt@10up.com>
2019-02-04 14:31:31 -0600

    add --no-fail flag

--- composer_scan/cli.py
+++ composer_scan/cli.py
@@ -10,8 +10,10 @@ import os
 @click.command()
 @click.option('-f', help="composer.lock file to scan, defaults to file in current directory", type=click.Path(exists=False), default="composer.lock")
 @click.option('-v', help="Verbose output, show status of all plugins, if not set only outputs found vulnerabilities", is_flag=True)
+@click.option('--no-fail', help="even if vulnerabilities are found, exit 0 (emergency option to not fail CI pipelines)", envvar="COMPOSER_SCAN_NO_FAIL", is_flag=True)
+@click.option('--token', help="WPVulnDB api token", envvar="WPVULNDB_API_TOKEN")
 @click.pass_context
-def main(ctx, f, v):
+def main(ctx, f, v, no_fail, token):
     """Console script for composer_scan."""
     if 'CI' in os.environ:
         ctx.color = True
@@ -30,6 +32,10 @@ def main(ctx, f, v):
         sys.exit(1)
 
     found = scanFile(composer_obj, v)
+
+    if no_fail:
+        click.secho("no-fail flag set, exiting 0...")
+        sys.exit(0)
     sys.exit(found)
 
 
