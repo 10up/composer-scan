7f17f353834b171ed093dc4395bc26bbff839b1c
Jason DeWitt <jason.dewitt@10up.com>
2019-02-05 14:19:21 -0600

    Bump version: 0.2.0 → 1.0.0

--- composer_scan/__init__.py
+++ composer_scan/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Jason DeWitt"""
 __email__ = 'jason.dewitt@10up.com'
-__version__ = '0.2.0'
+__version__ = '1.0.0'
--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jasondewitt/composer_scan',
-    version='0.2.0',
+    version='1.0.0',
     zip_safe=False,
 )
