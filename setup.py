4652e99fac9d5fb165185e1ea83bfefee8202831
Jason DeWitt <jason.dewitt@10up.com>
2019-02-08 11:35:46 -0600

    Bump version: 1.2.2 → 1.2.3

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jasondewitt/composer_scan',
-    version='version='1.2.2'',
+    version='version='1.2.3'',
     zip_safe=False,
 )
