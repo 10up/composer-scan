cbf9d741832c6c6d378b9ccbd9366e0448951440
Jason DeWitt <jason.dewitt@10up.com>
2019-02-15 13:30:24 -0600

    remove history.md refrence from setup.py

--- setup.py
+++ setup.py
@@ -8,9 +8,6 @@ from setuptools import setup, find_packages
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.md') as history_file:
-    history = history_file.read()
-
 requirements = ['Click>=6.0',
                 'requests',
 
@@ -45,7 +42,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description_content_type="text/markdown",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='composer_scan',
     name='composer_scan',
